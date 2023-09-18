705da923d4fb750bc106ae5f2e76d2f963031653
Shobhnik13 <shobhnikw@gmail.com>
2023-09-18 07:46:21 +0530

    completed fecthpost backend and started home page frontend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,38 @@
 
-import { UserButton, useAuth } from '@clerk/nextjs'
+import PostCard from '@/components/PostCard'
+import { fetchPosts } from '@/lib/actions/post.actions'
+import { UserButton, currentUser, useAuth } from '@clerk/nextjs'
 
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
-  
+export default async function Home() {
+  const user=await currentUser()
+  const res=await fetchPosts(1,30);
+  // console.log(res)
   return (
-   <div className='head-text text-left'>
-    hme
-   </div>
+    <>
+    <h1 className='head-text text-left'>Home</h1>
+    <section className='mt-9 flex flex-col gap-10'>
+      {res.posts.length === 0?(
+        <p className='no-result'>Oops! No posts found</p>
+      ):(
+        <>
+        {res.posts.map((item)=>(
+          <PostCard
+          key={item._id}
+          id={item._id}
+          currentUserId={user?.id || ""}
+          parentId={item.parentId}
+          content={item.text}
+          author={item.author}
+          community={item.community}
+          createdAt={item.createdAt}
+          comments={item.children}/>
+        ))}
+        </>
+      )}
+    </section>
+    </>
   )
 }
--- lib/actions/post.actions.ts
+++ lib/actions/post.actions.ts
@@ -41,3 +41,55 @@ export async function createPost({text,author,communityId,path}:params){
 
 
 }
+
+//fetch posts
+export async function fetchPosts(pageNumber=1,pageSize=20){
+    connectionToDb()
+    try{
+        //calculate the no of posts to skip
+        // as the 1st page would be skipping no posts
+        //so pageNumber-1=1-1=0 
+        // and skipcount would be 0
+        //results in showing all posts
+        //and for the 2nd page 2-1=1*20=20
+        // there would be 20 posts to skip for 2nd page
+        //coz those 20 posts were shown at 1st page
+        const skipCount=(pageNumber-1)*pageSize
+
+
+        //fetch the post that have no parents
+        //only a single post
+        //top level post
+        //coz post with parent are comments
+        //sorting it in desc order so that newest comees at top
+        const postQuery=Post.find({parentId:{$in: [null,undefined]}})
+        .sort({createdAt:"desc"})
+        .skip(skipCount)
+        .limit(pageSize)
+        .populate({path:'author',model:'User'})
+        .populate({path:'children',
+            populate:{
+                path:'author',
+                model:'User',
+                select:'_id name parentId image'
+            }})
+            //total posts without parents
+            //same way as calculating postquery
+            //this will be used in pagination
+            const totalPosts=await Post.countDocuments({parentId:{$in:[null,undefined]}})
+            //executing the postQuery
+            const posts=await postQuery.exec()
+            //now checking that next page exists or not
+            //if the total post count would be > skipcount + post that are executed
+            //ex- we have 45 posts in totalPosts
+            //20 are executed on page-1 and those 20 are skipped
+            //so total>20+20
+            //means we are having more post than the posts that would be skipped + posts that are executed 
+            const isNext=totalPosts > skipCount + posts.length
+            //if isnext exist return a obj of isnext and the posts that needs to be executed in the next page
+            return {isNext,posts}
+    }catch(error:any){
+        throw new Error(`error fetching post:${error.message}`)
+    }
+
+}
